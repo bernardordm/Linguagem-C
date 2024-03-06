18ff471bd26181b43badb36a26805da652e5c1ae
Bernardo de Resende Marcelino <72499270+bernardordm@users.noreply.github.com>
2024-03-06 11:54:11 -0300

    Add files via upload

--- prj_soma/src/prj_soma.c
+++ prj_soma/src/prj_soma.c
@@ -1,31 +1,51 @@
-/*
- ============================================================================
- Name        : prj_soma.c
- Author      : 
- Version     :
- Copyright   : Your copyright notice
- Description : Hello World in C, Ansi-style
- ============================================================================
- */
+	/*
+	 ============================================================================
+	 Name        : prj_soma.c
+	 Author      :
+	 Version     :
+	 Copyright   : Your copyright notice
+	 Description : Hello World in C, Ansi-style
+	 ============================================================================
+	 */
 
-#include <stdio.h>
-#include <stdlib.h>
+	#include <stdio.h>
+	#include <stdlib.h>
 
-int main (void) {
-	int x, y, result, cont;
-	cont = 1;
+	int main(void) {
+		int x, y, result, cont;
+		cont = 1;
 
-	while (cont <= 10) {
-		printf("Entre com dois números para serem somados:");
-		fflush(stdout);
-		scanf("%d %d", &x, &y);
+		while (cont <= 3) {
+			printf("Entre com dois números para serem somados:");
+			fflush(stdout);
+			scanf("%d %d", &x, &y);
 
-		result = x + y;
+			result = x + y;
 
-		printf ("o resultado da soma é: %d\n", result);
+			printf("o resultado da soma é: %d\n", result);
 
-		cont = cont + 1;
+			cont = cont + 1;
+		}
+		for (cont = 1; cont >= 3; cont++) {
+			printf("Entre com os dois números para serem somados: ");
+			fflush(stdout);
+			scanf("%d %d", &x, &y);
+
+			result = x + y;
+
+			printf("o resultado da soma é: %d\n", result);
+
+		}
+		cont = 1;
+		do{
+			printf("Entre com os dois números para serem somados\n");
+			fflush(stdout);
+			scanf("%d %d", &x, &y);
+
+			result = x + y;
+			printf("o valor da soma é %d \n", result);
+			cont++;
+		  } while (cont <= 3);
+
+	   return 0;
 	}
-
-	return 0;
-}
