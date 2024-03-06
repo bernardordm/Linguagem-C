8258f4dd21748988a6d63061718546ab1ffa2c9e
Bernardo de Resende Marcelino <72499270+bernardordm@users.noreply.github.com>
2024-03-06 12:15:12 -0300

    Add files via upload

---
+++ exec1_imparoupar/exec1_imparoupar.c
@@ -0,0 +1,20 @@
+/*
+ ============================================================================
+ Name        : prj_soma.c
+ Author      :
+ Version     :
+ Copyright   : Your copyright notice
+ Description : Hello World in C, Ansi-style
+ ============================================================================
+ */
+
+#include <stdio.h>
+#include <stdlib.h>
+
+int main(void) {
+
+int x, result, cont;
+while (cont =>3) {
+	printf("Escolha um número para saber se é par ou ímpar:")
+}
+
